d5aa5f1ddb16cbc67031656b77f87a268ea3f635
uiuqM <wilque.coelho@mail.uft.edu.br>
2022-12-22 21:21:11 -0300

    User entity and resource

---
+++ src/main/java/com/projectcourse/project/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.projectcourse.project.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projectcourse.project.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "wilque", "wilque@gmail.com", "998092760", "www");
+		return ResponseEntity.ok().body(u);
+	}
+}
