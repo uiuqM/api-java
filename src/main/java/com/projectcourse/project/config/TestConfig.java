4bd5b854b927c1d20a0bb1dee5d3f0dffac746be
uiuqM <wilque.coelho@mail.uft.edu.br>
2023-01-11 16:02:11 -0300

    Category

--- src/main/java/com/projectcourse/project/config/TestConfig.java
+++ src/main/java/com/projectcourse/project/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.projectcourse.project.entities.Category;
 import com.projectcourse.project.entities.Order;
 import com.projectcourse.project.entities.User;
 import com.projectcourse.project.entities.enums.OrderStatus;
+import com.projectcourse.project.repositories.CategoryRepository;
 import com.projectcourse.project.repositories.OrderRepository;
 import com.projectcourse.project.repositories.UserRepository;
 
@@ -22,8 +24,15 @@ public class TestConfig implements CommandLineRunner{
 	private UserRepository userRepository;
 	@Autowired
 	private OrderRepository orderRepository;
+	@Autowired
+	private CategoryRepository categoryRepository;
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Eletronics");
+		Category cat2 = new Category(null, "Books");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 		
 		User u1 = new User("Nyelle Ferreira Mota", "nyelle@gmail.com", "999096645", "010203");
 		User u2 = new User("Anna Julia", "bu@gmail.com", "998154321", "bubuzinha");
