e6c940176d5922f6fea2c0e4dcde9e3acd5dd248
uiuqM <wilque.coelho@mail.uft.edu.br>
2023-01-31 09:58:07 -0300

    OrderItem, many-to-many

--- src/main/java/com/projectcourse/project/config/TestConfig.java
+++ src/main/java/com/projectcourse/project/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.projectcourse.project.entities.Category;
 import com.projectcourse.project.entities.Order;
+import com.projectcourse.project.entities.OrderItem;
 import com.projectcourse.project.entities.Product;
 import com.projectcourse.project.entities.User;
 import com.projectcourse.project.entities.enums.OrderStatus;
 import com.projectcourse.project.repositories.CategoryRepository;
+import com.projectcourse.project.repositories.OrderItemRepository;
 import com.projectcourse.project.repositories.OrderRepository;
 import com.projectcourse.project.repositories.ProductRepository;
 import com.projectcourse.project.repositories.UserRepository;
@@ -30,6 +32,9 @@ public class TestConfig implements CommandLineRunner{
 	private CategoryRepository categoryRepository;
 	@Autowired
 	private ProductRepository productRepository;
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -62,6 +67,11 @@ public class TestConfig implements CommandLineRunner{
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p2, 1, p2.getPrice());
+		OrderItem oi2 = new OrderItem(o2, p1, 2, p1.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2));
 	}
 	
 	
