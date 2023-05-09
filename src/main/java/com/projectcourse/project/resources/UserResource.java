a3a58d6cac3da2395bfa2604864093b9d5ebdb6f
uiuqM <wilque.coelho@mail.uft.edu.br>
2023-05-09 02:13:11 -0300

    remove unused

--- src/main/java/com/projectcourse/project/resources/UserResource.java
+++ src/main/java/com/projectcourse/project/resources/UserResource.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.util.UriComponents.UriTemplateVariables;
 
 import com.projectcourse.project.entities.User;
 import com.projectcourse.project.services.UserService;
