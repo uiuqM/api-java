4f090c1d8f03cea0d3fd182bd8ff6701e55165d1
uiuqM <wilque.coelho@mail.uft.edu.br>
2023-05-13 14:04:16 -0300

    CRUD options to products

--- src/main/java/com/projectcourse/project/services/ProductService.java
+++ src/main/java/com/projectcourse/project/services/ProductService.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.projectcourse.project.entities.Product;
 import com.projectcourse.project.repositories.ProductRepository;
+import com.projectcourse.project.services.exceptions.DatabaseException;
+import com.projectcourse.project.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ProductService {
@@ -21,5 +27,36 @@ public class ProductService {
 	public Product findById(Long id) {
 		Optional<Product> obj = repository.findById(id);
 		return obj.get();
+	}
+	public Product insert(Product obj){
+		return repository.save(obj);
+	}
+
+	public void delete(Long id){
+		try{
+			repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e){
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e){
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+
+	public Product update(Long id, Product obj){
+		try{
+			Product entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		}catch(EntityNotFoundException e){
+			throw new ResourceNotFoundException(id);
+		}
+		
+	}
+
+	private void updateData(Product entity, Product obj){
+		entity.setName(obj.getName());
+		entity.setDescription(obj.getDescription());
+		entity.setPrice(obj.getPrice());
+		entity.setimgUrl(obj.getImgUrl());
 	}
 }
